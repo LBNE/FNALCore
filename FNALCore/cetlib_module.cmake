53a80f57b9c97566c6f965d3a3c48f4862d34482
Ben Morgan <bmorgan.warwick@gmail.com>
2014-09-05 15:57:52 +0100

    Build cpp0x module using new subtree structure
    Update paths in cetlib_module.cmake to reflect subtree structure.
    Use SOURCES argument in cet_test calls in cetlib_module_tests.cmake
    as cet_test assumes it will be called in the same directory where the test
    source exists.
    
    Add cetlib OBJECT library and headers paths to build of FNALCore.
    
    Note that three tests currently fail:
    
    - value_ptr_test_2: Due to a GCC-ism in the code
    - LibraryManager_t: due to library lookup path issues
    - PluginManager_t: due to library lookup issues

--- FNALCore/cetlib_module.cmake
+++ FNALCore/cetlib_module.cmake
@@ -3,98 +3,101 @@
 # Build/install script
 #
 # ======================================================================
-configure_file(${CMAKE_CURRENT_SOURCE_DIR}/shlib_utils.cc.in
+set(cetlib_ROOT_DIR "${CMAKE_CURRENT_SOURCE_DIR}/cetlib")
+set(cetlib_INCLUDE_DIR "${cetlib_ROOT_DIR}/cetlib")
+
+configure_file(${cetlib_INCLUDE_DIR}/shlib_utils.cc.in
   ${CMAKE_CURRENT_BINARY_DIR}/shlib_utils.cc @ONLY
   )
 
 set(cetlib_PUBLIC_HDRS
-  base_converter.h
-  BasicPluginFactory.h
-  bit_manipulation.h
-  canonical_number.h
-  canonical_string.h
-  coded_exception.h
-  column_width.h
-  container_algorithms.h
-  cpu_timer.h
-  crc32.h
-  demangle.h
-  exception_collector.h
-  exception.h
-  exempt_ptr.h
-  filepath_maker.h
-  filesystem.h
-  getenv.h
-  hard_cast.h
-  hypot.h
-  include.h
-  includer.h
-  LibraryManager.h
-  lpad.h
-  make_unique.h
-  map_vector.h
-  maybe_ref.h
-  md5.h
-  name_of.h
-  ntos.h
-  nybbler.h
-  PluginFactory.h
-  PluginTypeDeducer.h
-  pow.h
-  registry.h
-  registry_via_id.h
-  rpad.h
-  search_path.h
-  sha1.h
-  shlib_utils.h
-  simple_stats.h
-  split.h
-  split_path.h
-  ston.h
-  trim.h
-  value_ptr.h
-  zero_init.h
+  ${cetlib_INCLUDE_DIR}/base_converter.h
+  ${cetlib_INCLUDE_DIR}/BasicPluginFactory.h
+  ${cetlib_INCLUDE_DIR}/bit_manipulation.h
+  ${cetlib_INCLUDE_DIR}/canonical_number.h
+  ${cetlib_INCLUDE_DIR}/canonical_string.h
+  ${cetlib_INCLUDE_DIR}/coded_exception.h
+  ${cetlib_INCLUDE_DIR}/column_width.h
+  ${cetlib_INCLUDE_DIR}/container_algorithms.h
+  ${cetlib_INCLUDE_DIR}/cpu_timer.h
+  ${cetlib_INCLUDE_DIR}/crc32.h
+  ${cetlib_INCLUDE_DIR}/demangle.h
+  ${cetlib_INCLUDE_DIR}/exception_collector.h
+  ${cetlib_INCLUDE_DIR}/exception.h
+  ${cetlib_INCLUDE_DIR}/exempt_ptr.h
+  ${cetlib_INCLUDE_DIR}/filepath_maker.h
+  ${cetlib_INCLUDE_DIR}/filesystem.h
+  ${cetlib_INCLUDE_DIR}/getenv.h
+  ${cetlib_INCLUDE_DIR}/hard_cast.h
+  ${cetlib_INCLUDE_DIR}/hypot.h
+  ${cetlib_INCLUDE_DIR}/include.h
+  ${cetlib_INCLUDE_DIR}/includer.h
+  ${cetlib_INCLUDE_DIR}/LibraryManager.h
+  ${cetlib_INCLUDE_DIR}/lpad.h
+  ${cetlib_INCLUDE_DIR}/make_unique.h
+  ${cetlib_INCLUDE_DIR}/map_vector.h
+  ${cetlib_INCLUDE_DIR}/maybe_ref.h
+  ${cetlib_INCLUDE_DIR}/md5.h
+  ${cetlib_INCLUDE_DIR}/name_of.h
+  ${cetlib_INCLUDE_DIR}/ntos.h
+  ${cetlib_INCLUDE_DIR}/nybbler.h
+  ${cetlib_INCLUDE_DIR}/PluginFactory.h
+  ${cetlib_INCLUDE_DIR}/PluginTypeDeducer.h
+  ${cetlib_INCLUDE_DIR}/pow.h
+  ${cetlib_INCLUDE_DIR}/registry.h
+  ${cetlib_INCLUDE_DIR}/registry_via_id.h
+  ${cetlib_INCLUDE_DIR}/rpad.h
+  ${cetlib_INCLUDE_DIR}/search_path.h
+  ${cetlib_INCLUDE_DIR}/sha1.h
+  ${cetlib_INCLUDE_DIR}/shlib_utils.h
+  ${cetlib_INCLUDE_DIR}/simple_stats.h
+  ${cetlib_INCLUDE_DIR}/split.h
+  ${cetlib_INCLUDE_DIR}/split_path.h
+  ${cetlib_INCLUDE_DIR}/ston.h
+  ${cetlib_INCLUDE_DIR}/trim.h
+  ${cetlib_INCLUDE_DIR}/value_ptr.h
+  ${cetlib_INCLUDE_DIR}/zero_init.h
   )
 
 set(cetlib_DETAIL_HDRS
-  detail/wrapLibraryManagerException.h
+  ${cetlib_INCLUDE_DIR}/detail/wrapLibraryManagerException.h
   )
 
 set(cetlib_SSL_HDRS
-  polarssl/config.h
-  polarssl/md5.h
-  polarssl/sha1.h
-  polarssl/version.h
+  ${cetlib_ROOT_DIR}/polarssl/config.h
+  ${cetlib_ROOT_DIR}/polarssl/md5.h
+  ${cetlib_ROOT_DIR}/polarssl/sha1.h
+  ${cetlib_ROOT_DIR}/polarssl/version.h
   )
 
 set(cetlib_SRCS
   ${CMAKE_CURRENT_BINARY_DIR}/shlib_utils.cc
-  detail/wrapLibraryManagerException.cc
-  polarssl/md5.cc
-  polarssl/sha1.cc
-  base_converter.cc
-  BasicPluginFactory.cc
-  canonical_number.cc
-  canonical_string.cc
-  cpu_timer.cc
-  crc32.cc
-  demangle.cc
-  exception.cc
-  exception_collector.cc
-  filepath_maker.cc
-  filesystem.cc
-  getenv.cc
-  inc-expand.cc
-  include.cc
-  includer.cc
-  LibraryManager.cc
-  lpad.cc
-  nybbler.cc
-  PluginFactory.cc
-  rpad.cc
-  search_path.cc
-  simple_stats.cc
-  split_path.cc
+  ${cetlib_INCLUDE_DIR}/detail/wrapLibraryManagerException.cc
+  ${cetlib_ROOT_DIR}/polarssl/md5.cc
+  ${cetlib_ROOT_DIR}/polarssl/sha1.cc
+  ${cetlib_INCLUDE_DIR}/base_converter.cc
+  ${cetlib_INCLUDE_DIR}/BasicPluginFactory.cc
+  ${cetlib_INCLUDE_DIR}/canonical_number.cc
+  ${cetlib_INCLUDE_DIR}/canonical_string.cc
+  ${cetlib_INCLUDE_DIR}/cpu_timer.cc
+  ${cetlib_INCLUDE_DIR}/crc32.cc
+  ${cetlib_INCLUDE_DIR}/demangle.cc
+  ${cetlib_INCLUDE_DIR}/exception.cc
+  ${cetlib_INCLUDE_DIR}/exception_collector.cc
+  ${cetlib_INCLUDE_DIR}/filepath_maker.cc
+  ${cetlib_INCLUDE_DIR}/filesystem.cc
+  ${cetlib_INCLUDE_DIR}/getenv.cc
+  ${cetlib_INCLUDE_DIR}/inc-expand.cc
+  ${cetlib_INCLUDE_DIR}/include.cc
+  ${cetlib_INCLUDE_DIR}/includer.cc
+  ${cetlib_INCLUDE_DIR}/LibraryManager.cc
+  ${cetlib_INCLUDE_DIR}/lpad.cc
+  ${cetlib_INCLUDE_DIR}/nybbler.cc
+  ${cetlib_INCLUDE_DIR}/PluginFactory.cc
+  ${cetlib_INCLUDE_DIR}/rpad.cc
+  ${cetlib_INCLUDE_DIR}/search_path.cc
+  ${cetlib_INCLUDE_DIR}/simple_stats.cc
+  ${cetlib_INCLUDE_DIR}/split_path.cc
   )
 
 include_directories(${CMAKE_CURRENT_SOURCE_DIR})
@@ -122,7 +125,7 @@ install(FILES ${cetlib_SSL_HDRS}
 #-----------------------------------------------------------------------
 # Testing
 if(FNALCore_ENABLE_TESTING)
-  add_subdirectory(unittests)
+  include(cetlib_module_tests.cmake)
 endif()
 
 
