9cb8a7d832c0a11ade819c2388b8177b9cb130d4
Ben Morgan <bmorgan.warwick@gmail.com>
2014-09-05 17:21:00 +0100

    Build messagefacility module using subtree structure
    Update paths in messagefacility_module.cmake script to reflect new
    subtree structure. Add resultant OBJECT library into FNALCore build,
    and add include paths to INTERFACE definitions.
    
    No tests are yet implemented for messagefacility as their structure
    under the subtree is complex, with possibly obsolete tests.
    
    NB: Build has not been verified in this commit because of errors
    in building with Clang on OSX Mavericks:
    
    - messagefacility/MessageService/ELtsErrorLog.icc causes issues
    due to returns and protected visibility.
    
    May work on FNAL gcc platforms...

--- FNALCore/messagefacility_module.cmake
+++ FNALCore/messagefacility_module.cmake
@@ -4,130 +4,135 @@
 # Also not obvious if there is any split between public/private headers
 # Some direct use of C++11 headers as well.
 
+# - Base directories
+set(messagefacility_ROOT_DIR "${CMAKE_CURRENT_SOURCE_DIR}/messagefacility")
+set(messagefacility_INCLUDE_DIR "${messagefacility_ROOT_DIR}/messagefacility")
+
+
 #-----------------------------------------------------------------------
 # Utilities
 #
 set(MessageFacility_Utilities_HEADERS
-  Utilities/DebugMacros.h
-  Utilities/do_nothing_deleter.h
-  Utilities/exception.h
-  Utilities/FormatTime.h
-  Utilities/JobMode.h
-  Utilities/SingleConsumerQ.h
-  Utilities/tinystr.h
-  Utilities/tinyxml.h
-  Utilities/UnixSignalHandlers.h
+  ${messagefacility_INCLUDE_DIR}/Utilities/DebugMacros.h
+  ${messagefacility_INCLUDE_DIR}/Utilities/do_nothing_deleter.h
+  ${messagefacility_INCLUDE_DIR}/Utilities/exception.h
+  ${messagefacility_INCLUDE_DIR}/Utilities/FormatTime.h
+  ${messagefacility_INCLUDE_DIR}/Utilities/JobMode.h
+  ${messagefacility_INCLUDE_DIR}/Utilities/SingleConsumerQ.h
+  ${messagefacility_INCLUDE_DIR}/Utilities/tinystr.h
+  ${messagefacility_INCLUDE_DIR}/Utilities/tinyxml.h
+  ${messagefacility_INCLUDE_DIR}/Utilities/UnixSignalHandlers.h
   )
 set(MessageFacility_Utilities_SOURCES
-  Utilities/DebugMacros.cc
-  Utilities/exception.cc
-  Utilities/FormatTime.cc
-  Utilities/SingleConsumerQ.cc
-  Utilities/tinyxml.cc
-  Utilities/tinyxmlerror.cc
-  Utilities/tinyxmlparser.cc
-  Utilities/UnixSignalHandlers.cc
+  ${messagefacility_INCLUDE_DIR}/Utilities/DebugMacros.cc
+  ${messagefacility_INCLUDE_DIR}/Utilities/exception.cc
+  ${messagefacility_INCLUDE_DIR}/Utilities/FormatTime.cc
+  ${messagefacility_INCLUDE_DIR}/Utilities/SingleConsumerQ.cc
+  ${messagefacility_INCLUDE_DIR}/Utilities/tinyxml.cc
+  ${messagefacility_INCLUDE_DIR}/Utilities/tinyxmlerror.cc
+  ${messagefacility_INCLUDE_DIR}/Utilities/tinyxmlparser.cc
+  ${messagefacility_INCLUDE_DIR}/Utilities/UnixSignalHandlers.cc
   )
 
 #-----------------------------------------------------------------------
 # Logger/Service
 #
 set(MessageService_HEADERS
-  MessageService/ELadministrator.h
-  MessageService/ELassertN.h
-  MessageService/ELcollected.h
-  MessageService/ELcontextSupplier.h
-  MessageService/ELdestControl.h
-  MessageService/ELdestination.h
-  MessageService/ELdestMakerMacros.h
-  MessageService/ELerrorList.h
-  MessageService/ELfwkJobReport.h
-  MessageService/ELlimitsTable.h
-  MessageService/ELoutput.h
-  MessageService/ELrecv.h
-  MessageService/ELsender.h
-  MessageService/ELset.h
-  MessageService/ELstatistics.h
-  MessageService/ELtsErrorLog.h
-  MessageService/ELtsErrorLog.icc
-  MessageService/ErrorLog.h
-  MessageService/ErrorLog.icc
-  MessageService/MainThreadMLscribe.h
-  MessageService/MessageLoggerDefaults.h
-  MessageService/MessageLogger.h
-  MessageService/MessageLoggerScribe.h
-  MessageService/MessageServicePresence.h
-  MessageService/MsgContext.h
-  MessageService/NamedDestination.h
-  MessageService/Presence.h
-  MessageService/SingleThreadMSPresence.h
-  MessageService/ThreadQueue.h
-  MessageService/ThreadSafeErrorLog.h
+  ${messagefacility_INCLUDE_DIR}/MessageService/ELadministrator.h
+  ${messagefacility_INCLUDE_DIR}/MessageService/ELassertN.h
+  ${messagefacility_INCLUDE_DIR}/MessageService/ELcollected.h
+  ${messagefacility_INCLUDE_DIR}/MessageService/ELcontextSupplier.h
+  ${messagefacility_INCLUDE_DIR}/MessageService/ELdestControl.h
+  ${messagefacility_INCLUDE_DIR}/MessageService/ELdestination.h
+  ${messagefacility_INCLUDE_DIR}/MessageService/ELdestMakerMacros.h
+  ${messagefacility_INCLUDE_DIR}/MessageService/ELerrorList.h
+  ${messagefacility_INCLUDE_DIR}/MessageService/ELfwkJobReport.h
+  ${messagefacility_INCLUDE_DIR}/MessageService/ELlimitsTable.h
+  ${messagefacility_INCLUDE_DIR}/MessageService/ELoutput.h
+  ${messagefacility_INCLUDE_DIR}/MessageService/ELrecv.h
+  ${messagefacility_INCLUDE_DIR}/MessageService/ELsender.h
+  ${messagefacility_INCLUDE_DIR}/MessageService/ELset.h
+  ${messagefacility_INCLUDE_DIR}/MessageService/ELstatistics.h
+  ${messagefacility_INCLUDE_DIR}/MessageService/ELtsErrorLog.h
+  ${messagefacility_INCLUDE_DIR}/MessageService/ELtsErrorLog.icc
+  ${messagefacility_INCLUDE_DIR}/MessageService/ErrorLog.h
+  ${messagefacility_INCLUDE_DIR}/MessageService/ErrorLog.icc
+  ${messagefacility_INCLUDE_DIR}/MessageService/MainThreadMLscribe.h
+  ${messagefacility_INCLUDE_DIR}/MessageService/MessageLoggerDefaults.h
+  ${messagefacility_INCLUDE_DIR}/MessageService/MessageLogger.h
+  ${messagefacility_INCLUDE_DIR}/MessageService/MessageLoggerScribe.h
+  ${messagefacility_INCLUDE_DIR}/MessageService/MessageServicePresence.h
+  ${messagefacility_INCLUDE_DIR}/MessageService/MsgContext.h
+  ${messagefacility_INCLUDE_DIR}/MessageService/NamedDestination.h
+  ${messagefacility_INCLUDE_DIR}/MessageService/Presence.h
+  ${messagefacility_INCLUDE_DIR}/MessageService/SingleThreadMSPresence.h
+  ${messagefacility_INCLUDE_DIR}/MessageService/ThreadQueue.h
+  ${messagefacility_INCLUDE_DIR}/MessageService/ThreadSafeErrorLog.h
   )
 set(MessageLogger_HEADERS
-  MessageLogger/AbstractMLscribe.h
-  MessageLogger/classes.h
-  MessageLogger/ConfigurationHandshake.h
-  MessageLogger/ELextendedID.h
-  MessageLogger/ELlist.h
-  MessageLogger/ELmap.h
-  MessageLogger/ELseverityLevel.h
-  MessageLogger/ELseverityLevel.icc
-  MessageLogger/ELstring.h
-  MessageLogger/ErrorObj.h
-  MessageLogger/ErrorObj.icc
-  MessageLogger/ErrorSummaryEntry.h
-  MessageLogger/ExceptionMessages.h
-  MessageLogger/JobReport.h
-  MessageLogger/LoggedErrorsSummary.h
-  MessageLogger/MessageDrop.h
-  MessageLogger/MessageFacilityMsg.h
-  MessageLogger/MessageLogger.h
-  MessageLogger/MessageLoggerQ.h
-  MessageLogger/MessageSender.h
-  MessageLogger/SilentMLscribe.h
-  MessageLogger/ThreadSafeErrorLog.icc
+  ${messagefacility_INCLUDE_DIR}/MessageLogger/AbstractMLscribe.h
+  ${messagefacility_INCLUDE_DIR}/MessageLogger/classes.h
+  ${messagefacility_INCLUDE_DIR}/MessageLogger/ConfigurationHandshake.h
+  ${messagefacility_INCLUDE_DIR}/MessageLogger/ELextendedID.h
+  ${messagefacility_INCLUDE_DIR}/MessageLogger/ELlist.h
+  ${messagefacility_INCLUDE_DIR}/MessageLogger/ELmap.h
+  ${messagefacility_INCLUDE_DIR}/MessageLogger/ELseverityLevel.h
+  ${messagefacility_INCLUDE_DIR}/MessageLogger/ELseverityLevel.icc
+  ${messagefacility_INCLUDE_DIR}/MessageLogger/ELstring.h
+  ${messagefacility_INCLUDE_DIR}/MessageLogger/ErrorObj.h
+  ${messagefacility_INCLUDE_DIR}/MessageLogger/ErrorObj.icc
+  ${messagefacility_INCLUDE_DIR}/MessageLogger/ErrorSummaryEntry.h
+  ${messagefacility_INCLUDE_DIR}/MessageLogger/ExceptionMessages.h
+  ${messagefacility_INCLUDE_DIR}/MessageLogger/JobReport.h
+  ${messagefacility_INCLUDE_DIR}/MessageLogger/LoggedErrorsSummary.h
+  ${messagefacility_INCLUDE_DIR}/MessageLogger/MessageDrop.h
+  ${messagefacility_INCLUDE_DIR}/MessageLogger/MessageFacilityMsg.h
+  ${messagefacility_INCLUDE_DIR}/MessageLogger/MessageLogger.h
+  ${messagefacility_INCLUDE_DIR}/MessageLogger/MessageLoggerQ.h
+  ${messagefacility_INCLUDE_DIR}/MessageLogger/MessageSender.h
+  ${messagefacility_INCLUDE_DIR}/MessageLogger/SilentMLscribe.h
+  ${messagefacility_INCLUDE_DIR}/MessageLogger/ThreadSafeErrorLog.icc
   )
 set(MessageFacility_HEADERS ${MessageService_HEADERS} ${MessageLogger_HEADERS})
 
 set(MessageFacility_SOURCES
-  MessageService/ELadministrator.cc
-  MessageService/ELcollected.cc
-  MessageService/ELdestControl.cc
-  MessageService/ELdestination.cc
-  MessageService/ELerrorList.cc
-  MessageService/ELfwkJobReport.cc
-  MessageService/ELlimitsTable.cc
-  MessageService/ELoutput.cc
-  MessageService/ELrecv.cc
-  MessageService/ELstatistics.cc
-  MessageService/ELtsErrorLog.cc
-  MessageService/ErrorLog.cc
-  MessageService/HardwiredDefaults.cc
-  MessageService/MainThreadMLscribe.cc
-  MessageService/MessageLogger.cc
-  MessageService/MessageLoggerDefaults.cc
-  MessageService/MessageLoggerScribe.cc
-  MessageService/MessageServicePresence.cc
-  MessageService/MsgContext.cc
-  MessageService/Presence.cc
-  MessageService/SingleThreadMSPresence.cc
-  MessageService/ThreadQueue.cc
-  MessageLogger/AbstractMLscribe.cc
-  MessageLogger/ELextendedID.cc
-  MessageLogger/ELmap.cc
-  MessageLogger/ELseverityLevel.cc
-  MessageLogger/ELstring.cc
-  MessageLogger/ErrorObj.cc
-  MessageLogger/ExceptionMessages.cc
-  MessageLogger/JobReport.cc
-  MessageLogger/LoggedErrorsSummary.cc
-  MessageLogger/MessageDrop.cc
-  MessageLogger/MessageFacilityMsg.cc
-  MessageLogger/MessageLogger.cc
-  MessageLogger/MessageLoggerQ.cc
-  MessageLogger/MessageSender.cc
-  MessageLogger/SilentMLscribe.cc
+  ${messagefacility_INCLUDE_DIR}/MessageService/ELadministrator.cc
+  ${messagefacility_INCLUDE_DIR}/MessageService/ELcollected.cc
+  ${messagefacility_INCLUDE_DIR}/MessageService/ELdestControl.cc
+  ${messagefacility_INCLUDE_DIR}/MessageService/ELdestination.cc
+  ${messagefacility_INCLUDE_DIR}/MessageService/ELerrorList.cc
+  ${messagefacility_INCLUDE_DIR}/MessageService/ELfwkJobReport.cc
+  ${messagefacility_INCLUDE_DIR}/MessageService/ELlimitsTable.cc
+  ${messagefacility_INCLUDE_DIR}/MessageService/ELoutput.cc
+  ${messagefacility_INCLUDE_DIR}/MessageService/ELrecv.cc
+  ${messagefacility_INCLUDE_DIR}/MessageService/ELstatistics.cc
+  ${messagefacility_INCLUDE_DIR}/MessageService/ELtsErrorLog.cc
+  ${messagefacility_INCLUDE_DIR}/MessageService/ErrorLog.cc
+  ${messagefacility_INCLUDE_DIR}/MessageService/HardwiredDefaults.cc
+  ${messagefacility_INCLUDE_DIR}/MessageService/MainThreadMLscribe.cc
+  ${messagefacility_INCLUDE_DIR}/MessageService/MessageLogger.cc
+  ${messagefacility_INCLUDE_DIR}/MessageService/MessageLoggerDefaults.cc
+  ${messagefacility_INCLUDE_DIR}/MessageService/MessageLoggerScribe.cc
+  ${messagefacility_INCLUDE_DIR}/MessageService/MessageServicePresence.cc
+  ${messagefacility_INCLUDE_DIR}/MessageService/MsgContext.cc
+  ${messagefacility_INCLUDE_DIR}/MessageService/Presence.cc
+  ${messagefacility_INCLUDE_DIR}/MessageService/SingleThreadMSPresence.cc
+  ${messagefacility_INCLUDE_DIR}/MessageService/ThreadQueue.cc
+  ${messagefacility_INCLUDE_DIR}/MessageLogger/AbstractMLscribe.cc
+  ${messagefacility_INCLUDE_DIR}/MessageLogger/ELextendedID.cc
+  ${messagefacility_INCLUDE_DIR}/MessageLogger/ELmap.cc
+  ${messagefacility_INCLUDE_DIR}/MessageLogger/ELseverityLevel.cc
+  ${messagefacility_INCLUDE_DIR}/MessageLogger/ELstring.cc
+  ${messagefacility_INCLUDE_DIR}/MessageLogger/ErrorObj.cc
+  ${messagefacility_INCLUDE_DIR}/MessageLogger/ExceptionMessages.cc
+  ${messagefacility_INCLUDE_DIR}/MessageLogger/JobReport.cc
+  ${messagefacility_INCLUDE_DIR}/MessageLogger/LoggedErrorsSummary.cc
+  ${messagefacility_INCLUDE_DIR}/MessageLogger/MessageDrop.cc
+  ${messagefacility_INCLUDE_DIR}/MessageLogger/MessageFacilityMsg.cc
+  ${messagefacility_INCLUDE_DIR}/MessageLogger/MessageLogger.cc
+  ${messagefacility_INCLUDE_DIR}/MessageLogger/MessageLoggerQ.cc
+  ${messagefacility_INCLUDE_DIR}/MessageLogger/MessageSender.cc
+  ${messagefacility_INCLUDE_DIR}/MessageLogger/SilentMLscribe.cc
   )
 
 set_source_files_properties(${MessageFacility_HEADERS}
@@ -155,11 +160,4 @@ install(FILES ${MessageLogger_HEADERS}
   DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}/FNALCore/messagefacility/MessageLogger
   COMPONENT Development
   )
-
-#-----------------------------------------------------------------------
-# Testing
-if(FNALCore_ENABLE_TESTING)
-  add_subdirectory(unittests)
-endif()
-
 
